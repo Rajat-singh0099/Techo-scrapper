3e90872d86e005a114521044692967bb67fdf75e
Rajat-singh0099 <67358981+Rajat-singh0099@users.noreply.github.com>
2021-05-25 20:53:50 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -4,11 +4,16 @@ from bs4 import BeautifulSoup
 import time
 import tweepy
 import os
-import config
+from os import environ
+
+consumer_key = environ['consumer_key']
+consumer_secret_key = environ['consumer_secret_key']
+access_token = environ['access_token']
+access_token_secret = environ['access_token_secret']
 
 #authenticating to access the twitter API
-auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret_key)
-auth.set_access_token(config.access_token, config.access_token_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
